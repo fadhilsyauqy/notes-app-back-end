d11576fa942ecefb881d076db63245fd93713168
fadhil syauqy <rahmatfadhilsauqy08@gmail.com>
2024-11-10 13:02:05 +0700

    fix bugs host value

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import daStyle from 'eslint-config-dicodingacademy';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   daStyle,
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
